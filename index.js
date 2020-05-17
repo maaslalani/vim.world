141d9607d95e660a235151d42eef837033e8496b
Maas Lalani <maaslalani1@gmail.com>
2020-05-17 12:42:16 -0400

    express

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
