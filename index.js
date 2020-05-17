8132a067823ff4e6a2a49c4467c9d93cf90682f3
Maas Lalani <maaslalani1@gmail.com>
2020-05-17 12:49:42 -0400

    scaffold

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, () => {
